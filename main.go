729a4118e0c017e0e81b136d7e0e57935158778b
chenlianghong <clh021@gmail.com>
2022-01-12 19:30:08 +0800

    feat: env save path auto create

--- main.go
+++ main.go
@@ -5,16 +5,47 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 	"path/filepath"
 )
 
-var basePath = "/tmp/"
+var mem_fileSavePath string = ""
+
+func getFileSavePath() string {
+	if len(mem_fileSavePath) == 0 {
+		default_save_path := "/tmp/"
+		env_name := "MINI_TRANSFER_FILE_PATH"
+		env_path, exist := os.LookupEnv(env_name)
+		if !exist {
+			log.Printf("No env `%s` set. You can set like default `%s`", env_name, default_save_path)
+		} else {
+			if len(env_path) > 0 {
+				mem_fileSavePath = env_path
+				EnsureDir(filepath.Dir(env_path))
+				log.Printf("Use env `%s` get file save path: %s \n", env_name, env_path)
+			} else {
+				mem_fileSavePath = default_save_path
+				log.Printf("Empty env `%s` set to use default path: %s \n", env_name, default_save_path)
+			}
+		}
+	}
+	return mem_fileSavePath
+}
+
+func EnsureDir(baseDir string) error {
+	info, err := os.Stat(baseDir)
+	if err == nil && info.IsDir() {
+		return nil
+	}
+	return os.MkdirAll(baseDir, 0755)
+}
 
 func FileHandler(w http.ResponseWriter, r *http.Request) {
-	filePath := filepath.Join(basePath, r.URL.Path)
+	savePath := filepath.Join(getFileSavePath(), r.URL.Path)
+
 	if r.Method == http.MethodGet {
-		http.ServeFile(w, r, filePath)
-		log.Printf(" ▽ : %s\n", filePath)
+		http.ServeFile(w, r, savePath)
+		log.Printf(" ▽ : %s\n", savePath)
 	} else {
 		b, err := ioutil.ReadAll(r.Body)
 		if err != nil {
@@ -23,7 +54,7 @@ func FileHandler(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 
-		err = ioutil.WriteFile(filePath, b, 0644)
+		err = ioutil.WriteFile(savePath, b, 0644)
 		if err != nil {
 			w.WriteHeader(500)
 			_, _ = w.Write([]byte(fmt.Sprintf("%s", err)))
@@ -31,7 +62,7 @@ func FileHandler(w http.ResponseWriter, r *http.Request) {
 		}
 
 		w.WriteHeader(200)
-		_, _ = w.Write([]byte(fmt.Sprintf("OK %s", filePath)))
-		log.Printf(" △ : %s\n", filePath)
+		_, _ = w.Write([]byte(fmt.Sprintf("OK %s", savePath)))
+		log.Printf(" △ : %s\n", savePath)
 	}
 }
